825efe5319fb931ffb42dd50869d24000398dbf9
grypis <grypisnecros@gmail.com>
2024-10-10 14:47:19 +0300

    changes

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,18 +1,19 @@
 import mongoose from 'mongoose';
 import { env } from '../utils/env.js';
 
-export const initMongoConnection = async () => {
+export const initMongoCollection = async () => {
   try {
     const user = env('MONGODB_USER');
     const pwd = env('MONGODB_PASSWORD');
     const url = env('MONGODB_URL');
     const db = env('MONGODB_DB');
-    const connectionString = `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`;
-    await mongoose.connect(connectionString);
 
-    console.log('Mongo connection successfully established!');
+    await mongoose.connect(
+      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=MyCluster`,
+    );
+    console.log('Mongo connection successfully established');
   } catch (error) {
-    console.log('Error while setting up mongo connection', error.message);
+    console.log('Error while setting up mongo connection', error);
     throw error;
   }
 };
--- src/utils/env.js
+++ src/utils/env.js
@@ -1,9 +1,13 @@
 import dotenv from 'dotenv';
 
 dotenv.config();
-export const env = (name, defaultValue) => {
+
+export default function env(name, defaultValue) {
   const value = process.env[name];
+
   if (value) return value;
+
   if (defaultValue) return defaultValue;
+
   throw new Error(`Missing: process.env['${name}'].`);
-};
+}
